551c794009b9eb82d43a3d926115816091669d50
Muhammad Walid <152734820+Muhammad-walid@users.noreply.github.com>
2024-07-08 22:44:36 +0500

    This is my 4 day of coding.
    ### This File contains c++ code. And this code is all about patterns mean
    + "*" Patterns,
    +  Number patterns,
    + Alphabet patterns.
    ### >The purpose of this file is to build logic or thought process in programing. 

---
+++ patterens.cpp
@@ -0,0 +1,257 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        /* code
+
+    int n;
+    cin>>n;
+
+    int i = 1;
+    while (i<=n)
+    {
+        int j = 1;
+        while (j<=n)
+        {
+            cout<<j;
+            j++;
+            
+        }
+        cout<<endl;
+        i++;
+    }
+   
+
+
+    int n;
+    cin>>n;
+
+    int i = 1;
+    while (i<=n)
+    {
+        int j = 1;
+        while (j<=n)
+        {
+            cout<<n-j+1;
+            j++;
+            
+        }
+        cout<<endl;
+        i++;
+    }
+
+
+
+     int n;
+    cin>>n;
+
+    int i = 1;
+    int count = 1;
+    while (i<=n)
+    {
+        int j = 1;
+        while (j<=n)
+        {
+            cout<<count<<"  ";
+            count++;
+            j++;
+            
+        }
+        cout<<endl;
+        i++;
+
+    }
+       
+
+
+    int n;
+    cin>>n;
+    int i = 1;
+    while (i<=n)
+    {
+        int j = 1;
+        while (j<=i)
+        {
+            cout<<"* ";
+            j++;
+            
+        }
+        cout<<endl;
+        i++;
+    }
+    
+
+
+    int n;
+    cin>>n;
+    int i = 1;
+    while (i<=n)
+    {
+        int j = 1;
+        while (j<=i)
+        {
+            cout<<i;
+            j++;
+            
+        }
+        cout<<endl;
+        i++;
+    }
+
+    
+    int n;
+    cin>>n;
+    int i = 1;
+    int c = 1;
+    while (i<=n)
+    {
+        int j = 1;
+        while (j<=i)
+        {
+            cout<<c<<" ";
+            c++;
+            j++;
+            
+        }
+        cout<<endl;
+        i++;
+    }
+
+
+
+    int n;
+    cin>>n;
+    int i = 1;
+    
+    while (i<=n)
+    {
+        int j = 1;
+        int value = i;
+        while (j<=i)
+        {
+            cout<<value<<" ";
+            value++;
+            j++;
+            
+        }
+        cout<<endl;
+        i++;
+    }
+
+
+    int n;
+    cin>>n;
+    int i = 1;
+    
+    while (i<=n)
+    {
+        int j = 1;
+        while (j<=i)
+        {
+            cout<<i-j+1<<" ";
+            j++;
+            
+        }
+        cout<<endl;
+        i++;
+    }
+
+
+      int n;
+    cin>>n;
+    int i = 1;
+    
+    while (i<=n)
+    {
+        int j = 1;
+        while (j<=n)
+        {
+            char ch = 'A'+i-1;
+            cout<<ch<<" ";
+            j++;
+            
+        }
+        cout<<endl;
+        i++;
+    }
+
+
+
+        int n;
+    cin>>n;
+    int i = 1;
+    
+    while (i<=n)
+    {
+        int j = 1;
+        while (j<=n)
+        {
+            char ch = 'A'+j-1;
+            cout<<ch<<" ";
+            j++;
+            
+        }
+        cout<<endl;
+        i++;
+    }
+    
+
+
+    int n;
+    cin>>n;
+    int i = 1;
+    char value = 'A';
+    while (i<=n)
+    {
+        int j = 1;
+        while (j<=n)
+        {
+            
+            cout<<value<<" ";
+            value=value+1;
+            j++;
+            
+        }
+        cout<<endl;
+        i++;
+}
+
+
+ int n;
+    cin>>n;
+    int i = 1;
+
+    while (i<=n)
+    {
+        int j = 1;
+        while (j<=n)
+        {
+            char ch = 'A'+ i + j-2;
+            cout<<ch<<" ";
+            j++;
+            
+        }
+        cout<<endl;
+        i++;
+}
+*/
+
+
+ int n;
+    cin>>n;
+    int i = 1;
+
+    while (i<=n)
+    {
+        int j = 1;
+        while (j<=i)
+        {
+            char ch = 'A'+ i - 1;
+            cout<<ch<<" ";
+            j++;
+            
+        }
+        cout<<endl;
+        i++;
+}
+}
